e711d331e54604d2e78b6f5f396eb64dc6fa758c
derysudrajat <dery.sudrajat17@gmail.com>
2021-05-24 17:01:10 +0700

    add: some utils class it's Constants, DataHelpers, and DialogHelpers add: new utils method in Helpers file

---
+++ app/src/main/java/id/ruangopini/utils/DataHelpers.kt
@@ -0,0 +1,16 @@
+package id.ruangopini.utils
+
+object DataHelpers {
+
+    /** this
+     * @param errorLoginMessage using for placeholder message from firebase auth
+     */
+    val errorLoginMessage =
+        mapOf(
+            "There is no user record corresponding to this identifier. The user may have been deleted." to "User tidak ditemukan",
+            "The email address is badly formatted." to "Email tidak valid",
+            "The email address is already in use by another account." to "Email sudah terdaftar",
+            "The password is invalid or the user does not have a password." to "Password salah",
+            "The given password is invalid. [ Password should be at least 6 characters ]" to "Password paling sedikit 6 karakter"
+        )
+}

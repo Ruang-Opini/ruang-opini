0148bca51bdf61bf26add37cc4dd30367431ba2a
derysudrajat <dery.sudrajat17@gmail.com>
2021-05-24 17:05:56 +0700

    add: UploadPhotoActivity

---
+++ app/src/main/java/id/ruangopini/ui/register/uploadphoto/UploadPhotoActivity.kt
@@ -0,0 +1,70 @@
+package id.ruangopini.ui.register.uploadphoto
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
+import coil.load
+import coil.transform.CircleCropTransformation
+import com.github.dhaval2404.imagepicker.ImagePicker
+import id.ruangopini.MainActivity
+import id.ruangopini.databinding.ActivityUploadPhotoBinding
+import id.ruangopini.utils.Helpers.handleImagePicker
+
+class UploadPhotoActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityUploadPhotoBinding
+    private val model: UploadPhotoViewModel by viewModels()
+    private var photoUrl = ""
+
+// TODO: 5/24/2021 handle login with google
+//  private var isLoginWithGoogle = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityUploadPhotoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // TODO: 5/24/2021 getLasGoogleSign and populate it
+
+        with(binding) {
+            ivPicture.setOnClickListener {
+                ImagePicker.with(this@UploadPhotoActivity)
+                    .cropSquare()
+                    .createIntent { activityForResult.launch(it) }
+            }
+
+            button.setOnClickListener {
+                if (photoUrl.isNotBlank()) {
+                    model.uploadPhoto(photoUrl.toUri(), this@UploadPhotoActivity) { moveToMain() }
+                } else moveToMain()
+            }
+        }
+    }
+
+    private val activityForResult = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result: ActivityResult ->
+        result.handleImagePicker(applicationContext) {
+            photoUrl = it.toString()
+            with(binding) {
+                button.text = "Gunakan"
+                ivProfile.load(photoUrl) {
+                    crossfade(true)
+                    transformations(CircleCropTransformation())
+                }
+            }
+        }
+    }
+
+    private fun moveToMain() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finishAffinity()
+    }
+
+    override fun onBackPressed() { /*do nothing here*/
+    }
+}
---
+++ app/src/main/java/id/ruangopini/ui/register/uploadphoto/UploadPhotoViewModel.kt
@@ -0,0 +1,58 @@
+package id.ruangopini.ui.register.uploadphoto
+
+import android.app.Activity
+import android.net.Uri
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import id.ruangopini.data.repo.State
+import id.ruangopini.data.repo.remote.firebase.firestore.FirestoreUserRepository
+import id.ruangopini.data.repo.remote.firebase.storage.StorageUserRepository
+import id.ruangopini.utils.DialogHelpers
+import id.ruangopini.utils.Helpers
+import id.ruangopini.utils.PATH
+import id.ruangopini.utils.STORAGE
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+class UploadPhotoViewModel : ViewModel() {
+
+    // TODO: 5/24/2021 using koin dependency injection
+    private val storageUser = StorageUserRepository()
+    private val userRepo = FirestoreUserRepository()
+
+    fun uploadPhoto(uri: Uri, activity: Activity, isSuccess: () -> Unit) = viewModelScope.launch {
+        // TODO: 5/24/2021 handle if login with google
+        val uid = Firebase.auth.currentUser?.uid ?: ""
+        val path = PATH.AVA.plus(uid)
+        storageUser.uploadPhoto(uri, buildString {
+            append(STORAGE.ROOT_AVA).append(uid).append("/").append(path)
+        }).collect {
+            when (it) {
+                is State.Loading -> DialogHelpers.showLoadingDialog(activity, "Mengunggah Foto")
+                is State.Success -> updatePhoto(path, activity) { isSuccess() }
+                is State.Failed -> {
+                    DialogHelpers.hideLoadingDialog()
+                    Helpers.showToast(activity, it.message)
+                }
+            }
+        }
+    }
+
+    private fun updatePhoto(path: String, activity: Activity, isSuccess: () -> Unit) =
+        viewModelScope.launch {
+            userRepo.updatePhoto(path).collect {
+                when (it) {
+                    is State.Loading -> Log.d("TAG", "updatePhoto: loading")
+                    is State.Success -> isSuccess()
+                    is State.Failed -> {
+                        DialogHelpers.hideLoadingDialog()
+                        Helpers.showToast(activity, it.message)
+                    }
+                }
+            }
+        }
+
+}

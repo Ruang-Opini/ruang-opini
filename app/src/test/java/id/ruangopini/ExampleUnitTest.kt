6b7e8ad165bebe279cb3048033fbae2394877105
derysudrajat <dery.sudrajat17@gmail.com>
2021-05-26 15:49:11 +0700

    add: test logic for discussion

--- app/src/test/java/id/ruangopini/ExampleUnitTest.kt
+++ app/src/test/java/id/ruangopini/ExampleUnitTest.kt
@@ -1,6 +1,5 @@
 package id.ruangopini
 
-import org.junit.Assert.assertEquals
 import org.junit.Test
 
 /**
@@ -9,8 +8,30 @@ import org.junit.Test
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+    private data class Comment(
+        val comment: String? = null,
+        val discussionId: String? = null,
+        val postId: String? = null
+    )
+
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        val comments = listOf(
+            Comment("Waahhh Keren Banget", "AA", "aa"),
+            Comment("Anajay jiwaa", "BB", "bb"),
+            Comment("Loh koh bisa gitu", "AA", "aa"),
+            Comment("Aneh aja masa", "CC", "cc"),
+            Comment("Lagian ngapain coba bikin begituan", "AA", "aa"),
+            Comment("Yaampun", "AA", "aa"),
+            Comment("Wis angel angel", "CC", "cc"),
+            Comment("Ga ngerti lagi dah", "AA", "aa"),
+            Comment("Wahhh asik nih", "BB", "bb"),
+            Comment("Siap boss", "BB", "bb"),
+            Comment("Kawal terusss", "AA", "aa"),
+            Comment("Gaskeun broo", "AA", "aa"),
+        ).groupBy { it.discussionId }.forEach { (t, u) ->
+            println("t: $t, size = ${u.size}")
+            println("u: $u")
+        }
     }
 }

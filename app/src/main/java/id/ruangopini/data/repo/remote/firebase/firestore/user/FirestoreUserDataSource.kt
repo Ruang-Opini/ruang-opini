096155d609c8295d00b248c10c423614f28177d2
derysudrajat <dery.sudrajat17@gmail.com>
2021-05-30 14:23:08 +0700

    add: loginWithEmail, loginWithGoogle, forgotPassword in Auth repo add: getUserByUsername FirestoreUser repo

--- app/src/main/java/id/ruangopini/data/repo/remote/firebase/firestore/user/FirestoreUserDataSource.kt
+++ app/src/main/java/id/ruangopini/data/repo/remote/firebase/firestore/user/FirestoreUserDataSource.kt
@@ -8,4 +8,5 @@ interface FirestoreUserDataSource {
     fun createNewUser(user: User): Flow<State<Boolean>>
     fun updatePhoto(path: String): Flow<State<Boolean>>
     fun getUserById(userId: String): Flow<State<User>>
+    fun getUserByUsername(username: String): Flow<State<User>>
 }

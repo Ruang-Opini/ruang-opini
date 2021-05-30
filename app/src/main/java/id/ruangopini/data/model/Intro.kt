8e47ce683ee1c07748a38679ae088cb322725180
derysudrajat <dery.sudrajat17@gmail.com>
2021-05-30 14:20:01 +0700

    add: Intro model data class update: parcelize in PolicyCategory and User

--- app/src/main/java/id/ruangopini/data/model/Intro.kt
+++ app/src/main/java/id/ruangopini/data/model/Intro.kt
@@ -4,7 +4,8 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class PolicyCategory(
-    val name: String,
-    val url: String
+data class Intro(
+    val image: Int,
+    val title: String,
+    val subTitle: String,
 ) : Parcelable

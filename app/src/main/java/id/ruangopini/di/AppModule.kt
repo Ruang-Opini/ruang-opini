071c14ac173de8f13d37590ad4a94c074f6f16b4
derysudrajat <dery.sudrajat17@gmail.com>
2021-06-04 18:02:21 +0700

    add: trendingRetrofit, buzzerRetrofit in networkModule add: FirestorePostRepository, FirestoreCommentRepository in usecaseModule add: CreatePostViewModel, PostViewModel, DetailPostViewModel, DetailDiscussionViewModel, DetailTrendingPolicyViewModel in viewmodelModule

--- app/src/main/java/id/ruangopini/di/AppModule.kt
+++ app/src/main/java/id/ruangopini/di/AppModule.kt
@@ -4,12 +4,16 @@ import id.ruangopini.data.repo.IMainRepository
 import id.ruangopini.data.repo.MainRepository
 import id.ruangopini.data.repo.RemoteDataSource
 import id.ruangopini.data.repo.remote.firebase.auth.AuthRepository
+import id.ruangopini.data.repo.remote.firebase.firestore.comment.FirestoreCommentRepository
 import id.ruangopini.data.repo.remote.firebase.firestore.discussion.FirestoreDiscussionRepository
 import id.ruangopini.data.repo.remote.firebase.firestore.issue.FirestoreIssueRepository
+import id.ruangopini.data.repo.remote.firebase.firestore.post.FirestorePostRepository
 import id.ruangopini.data.repo.remote.firebase.firestore.user.FirestoreUserRepository
 import id.ruangopini.data.repo.remote.firebase.storage.StorageUserRepository
 import id.ruangopini.data.repo.remote.retrofit.ApiService
+import id.ruangopini.data.repo.remote.retrofit.BuzzerService
 import id.ruangopini.data.repo.remote.retrofit.SentimentService
+import id.ruangopini.data.repo.remote.retrofit.TrendingService
 import id.ruangopini.domain.MainInteract
 import id.ruangopini.domain.MainUseCase
 import id.ruangopini.ui.base.dicussion.DiscussionViewModel
@@ -17,8 +21,13 @@ import id.ruangopini.ui.base.home.HomeViewModel
 import id.ruangopini.ui.base.profile.ProfileViewModel
 import id.ruangopini.ui.base.reference.ReferenceViewModel
 import id.ruangopini.ui.discussion.create.CreateDiscussionViewModel
+import id.ruangopini.ui.discussion.detail.DetailDiscussionViewModel
 import id.ruangopini.ui.login.LoginViewModel
 import id.ruangopini.ui.policy.detail.DetailPolicyViewModel
+import id.ruangopini.ui.policy.trending.DetailTrendingPolicyViewModel
+import id.ruangopini.ui.post.create.CreatePostViewModel
+import id.ruangopini.ui.post.detail.DetailPostViewModel
+import id.ruangopini.ui.post.fragment.PostViewModel
 import id.ruangopini.ui.register.createaccount.CreateAccountViewModel
 import id.ruangopini.ui.register.uploadphoto.UploadPhotoViewModel
 import id.ruangopini.ui.settings.changepassword.ChangePasswordViewModel
@@ -50,16 +59,34 @@ val networkModule = module {
 
     single {
         val sentimentRetrofit = Retrofit.Builder()
-            .baseUrl("https://ropin-sentiment-gateway-7q7gq5ib.uc.gateway.dev/")
+            .baseUrl("https://offensive-gateway-7q7gq5ib.uc.gateway.dev/")
             .addConverterFactory(GsonConverterFactory.create())
             .client(get())
             .build()
         sentimentRetrofit.create(SentimentService::class.java)
     }
+
+    single {
+        val trendingRetrofit = Retrofit.Builder()
+            .baseUrl("https://trending-gateway-7q7gq5ib.uc.gateway.dev/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(get())
+            .build()
+        trendingRetrofit.create(TrendingService::class.java)
+    }
+
+    single {
+        val buzzerRetrofit = Retrofit.Builder()
+            .baseUrl("https://buzzer-gateway-7q7gq5ib.uc.gateway.dev/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(get())
+            .build()
+        buzzerRetrofit.create(BuzzerService::class.java)
+    }
 }
 
 val repoModule = module {
-    single { RemoteDataSource(get(), get()) }
+    single { RemoteDataSource(get(), get(), get(), get()) }
     single<IMainRepository> { MainRepository(get()) }
 }
 
@@ -70,6 +97,8 @@ val useCaseModule = module {
     single { AuthRepository() }
     single { StorageUserRepository() }
     single { FirestoreIssueRepository() }
+    single { FirestorePostRepository() }
+    single { FirestoreCommentRepository() }
 }
 
 val viewModelModule = module {
@@ -82,5 +111,10 @@ val viewModelModule = module {
     viewModel { ProfileViewModel(get(), get()) }
     viewModel { LoginViewModel(get(), get()) }
     viewModel { CreateDiscussionViewModel(get(), get()) }
-    viewModel { HomeViewModel(get()) }
+    viewModel { HomeViewModel(get(), get()) }
+    viewModel { CreatePostViewModel(get(), get(), get()) }
+    viewModel { PostViewModel(get()) }
+    viewModel { DetailPostViewModel(get(), get()) }
+    viewModel { DetailDiscussionViewModel(get()) }
+    viewModel { DetailTrendingPolicyViewModel(get()) }
 }

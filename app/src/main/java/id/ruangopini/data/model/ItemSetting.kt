33f34148dfc13ee555ca84274049dce814548e12
derysudrajat <dery.sudrajat17@gmail.com>
2021-06-06 10:21:53 +0700

    add: AboutUs, ItemHelp data class

--- app/src/main/java/id/ruangopini/data/model/ItemSetting.kt
+++ app/src/main/java/id/ruangopini/data/model/ItemSetting.kt
@@ -4,3 +4,15 @@ data class ItemSetting(
     val name: String,
     val target: Class<*>? = null
 )
+
+data class AboutUs(
+    val title: String,
+    val content: String
+)
+
+data class ItemHelp(
+    val question: String,
+    val answer: String,
+    val icons: List<String>,
+    var isExpand: Boolean? = false
+)
